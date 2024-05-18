e2bb0a66e6357e762c87ff462e872796120ceb09
ywerling <ywerling@hotmail.de>
2024-05-18 13:02:41 +0200

    Enable API call and receive image

--- main.py
+++ main.py
@@ -62,22 +62,23 @@ class TextToImageApp(tk.Frame):
         }
 
         # post API request
-        # response = requests.post(LIMEWIRE_ENDPOINT, json=payload, headers=headers)
-        # data = response.json()
-        # print(data)
+        response = requests.post(LIMEWIRE_ENDPOINT, json=payload, headers=headers)
+        data = response.json()
+        print(data)
 
         # image url is asset_url
 
         # sample response used to test without using API credits
-        data = {'id': '6286bc48-f936-4663-951b-2f3bd3a42d56',
-         'self': 'https://api.limewire.com/api/request/6286bc48-f936-4663-951b-2f3bd3a42d56', 'status': 'COMPLETED',
-         'failure_code': None, 'failure_reason': None, 'credits_used': 0.99, 'credits_remaining': 8.0199995, 'data': [
-            {'asset_id': 'f03b36ed-c9de-480e-b2dc-290c6608533e',
-             'self': 'https://api.limewire.com/api/assets/f03b36ed-c9de-480e-b2dc-290c6608533e',
-             'asset_url': 'https://ai-studio-assets.limewire.media/u/9ff0ef66-17d0-4ef0-9b96-cf1cea9e1709/image/11ef0397-8c2f-4136-99d9-35c3c98a8f4b?Expires=1716026695&Signature=LAoEc7UXVpgWYgHYRDmWKaFDvnM1XzB-BEvG1eQWD9xvZnT0-Ag75nL4Xv~vjs~8MAF~rvIE1QhmhFb87bCa8bZfyAf-Vr7rc1Q7ENF1sMivkINGTYITmXSKnnT6IvV93CPveTlsYjWjbPjofO7Mdz537mw1GBkVk94hsLjZI3w4NrWuWZfH74BASh2osW~ShfIeE0uVGbDrF8DrVF5z37uTCIBXhZaYX48eacvn6hIWPpqBzWGz57PBO1OtWkrmetK~O1rgrL4jwlEDlcJ21WyL3VzUuJon~j-1X1VqqqPsol4Eh7UjVtYVyBSibhnl2kc0XGIER2hXYY35~K2obg__&Key-Pair-Id=K1U52DHN9E92VT',
-             'type': 'image/jpeg', 'width': 1024, 'height': 1024}]}
-        print(data)
-        print(data['data'][0]['asset_url'])
+        # data = {'id': '6286bc48-f936-4663-951b-2f3bd3a42d56',
+        #  'self': 'https://api.limewire.com/api/request/6286bc48-f936-4663-951b-2f3bd3a42d56', 'status': 'COMPLETED',
+        #  'failure_code': None, 'failure_reason': None, 'credits_used': 0.99, 'credits_remaining': 8.0199995, 'data': [
+        #     {'asset_id': 'f03b36ed-c9de-480e-b2dc-290c6608533e',
+        #      'self': 'https://api.limewire.com/api/assets/f03b36ed-c9de-480e-b2dc-290c6608533e',
+        #      'asset_url': 'https://ai-studio-assets.limewire.media/u/9ff0ef66-17d0-4ef0-9b96-cf1cea9e1709/image/11ef0397-8c2f-4136-99d9-35c3c98a8f4b?Expires=1716026695&Signature=LAoEc7UXVpgWYgHYRDmWKaFDvnM1XzB-BEvG1eQWD9xvZnT0-Ag75nL4Xv~vjs~8MAF~rvIE1QhmhFb87bCa8bZfyAf-Vr7rc1Q7ENF1sMivkINGTYITmXSKnnT6IvV93CPveTlsYjWjbPjofO7Mdz537mw1GBkVk94hsLjZI3w4NrWuWZfH74BASh2osW~ShfIeE0uVGbDrF8DrVF5z37uTCIBXhZaYX48eacvn6hIWPpqBzWGz57PBO1OtWkrmetK~O1rgrL4jwlEDlcJ21WyL3VzUuJon~j-1X1VqqqPsol4Eh7UjVtYVyBSibhnl2kc0XGIER2hXYY35~K2obg__&Key-Pair-Id=K1U52DHN9E92VT',
+        #      'type': 'image/jpeg', 'width': 1024, 'height': 1024}]}
+        # print(data)
+        # print(data['data'][0]['asset_url'])
+
         self.image_url_entry.insert(0,data['data'][0]['asset_url'])
 
     def open_browser(self):
